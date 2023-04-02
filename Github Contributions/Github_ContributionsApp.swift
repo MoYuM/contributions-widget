bc13ef8c459801f98c4cab3827b8f2559575fc42
MoYuM <hanbaochang12138@gmail.com>
2023-04-02 16:22:43 +0800

    Initial Commit

---
+++ Github Contributions/Github_ContributionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ContributionsApp.swift
+//  Github Contributions
+//
+//  Created by 韩宝昌 on 2023/4/2.
+//
+
+import SwiftUI
+
+@main
+struct Github_ContributionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

0a9fdc89187c0ac3af55a8e17f04fb597bd89492
MoYuM <hanbaochang12138@gmail.com>
2023-04-08 18:41:39 +0800

    增加一些注释

--- contributions widget/contributions_widget.swift
+++ contributions widget/contributions_widget.swift
@@ -9,13 +9,30 @@ import WidgetKit
 import SwiftUI
 import Intents
 
+// 提供一系列供 widgetKit 使用的方法
+// placeholder: 在没有内容的时候，显示的东西
+// getSnapshot: 提供这个 widget 在小组件库中的预览样式
+// getTimeline:
+// widgetKit 按照 timeline 的策略去更新 widget，
+// getTimeline 就是提供这个 timeline 的方法，timeline 是包含了一系列 timelineEntry 的数组，
+// 每个 timelineEntry 代表了某个时间下，渲染 widget 所需要的数据。
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(
+            date: Date(),
+            today: Date(),
+            gridItemsDate: [[Date()]],
+            configuration: ConfigurationIntent()
+        )
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry =  SimpleEntry(
+            date: Date(),
+            today: Date(),
+            gridItemsDate: [[Date()]],
+            configuration: ConfigurationIntent()
+        )
         completion(entry)
     }
 
@@ -26,7 +43,12 @@ struct Provider: IntentTimelineProvider {
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = SimpleEntry(
+                date: entryDate,
+                today: Date(),
+                gridItemsDate: [[Date()]],
+                configuration: configuration
+            )
             entries.append(entry)
         }
 
@@ -35,14 +57,21 @@ struct Provider: IntentTimelineProvider {
     }
 }
 
+// 代表某个时间下，渲染 widget 所需要的数据
 struct SimpleEntry: TimelineEntry {
+    // 更新时间
     let date: Date
+    // 今天的日期
+    let today: Date
+    // 每个格子代表的日期
+    let gridItemsDate: [[Date]]
     let configuration: ConfigurationIntent
 }
 
+// 渲染
 struct contributions_widgetEntryView : View {
     var entry: Provider.Entry
-    @Environment(\.widgetFamily) var widgetFamily
+    @Environment(\.widgetFamily) var widgetFamily  // 环境变量
    
     var body: some View {
         let gridSize = self.getGridItemSize()
@@ -69,6 +98,7 @@ struct contributions_widgetEntryView : View {
        }
 }
 
+// 主入口
 struct contributions_widget: Widget {
     let kind: String = "contributions_widget"
 
@@ -84,13 +114,14 @@ struct contributions_widget: Widget {
         .description("This is an example widget.")
         .supportedFamilies([.systemLarge, .systemMedium, .systemSmall])
     }
-    
-    
 }
 
 struct contributions_widget_Previews: PreviewProvider {
     static var previews: some View {
-        contributions_widgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        contributions_widgetEntryView(
+            entry: SimpleEntry(date: Date(),
+            configuration: ConfigurationIntent())
+        )
             .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
